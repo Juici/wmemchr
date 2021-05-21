01cac3c9ab59e0f77b6eb36f35e5b8c8e6a8e082
James Whaley <juicy66173@gmail.com>
2021-05-21 17:17:46 +0100

    Fix issues with macro resolution

--- src/macros.rs
+++ src/macros.rs
@@ -8,57 +8,51 @@ macro_rules! static_assert {
     };
 }
 
-cfg_if::cfg_if! {
-    if #[cfg(rustc_1_53)] {
-        /// Search forward for the first set bit, in a non-zero bitmask.
-        macro_rules! bsf {
-            ($mask:expr) => {
-                match $mask {
-                    #[cfg(target_endian = "little")]
-                    mask => mask.trailing_zeros(),
-                    #[cfg(target_endian = "big")]
-                    mask => mask.leading_zeros(),
-                }
-            };
+/// Search forward for the first set bit, in a non-zero bitmask.
+#[cfg(rustc_1_53)]
+macro_rules! bsf {
+    ($mask:expr) => {
+        match $mask {
+            #[cfg(target_endian = "little")]
+            mask => mask.trailing_zeros(),
+            #[cfg(target_endian = "big")]
+            mask => mask.leading_zeros(),
         }
-    } else {
-        /// Search forward for the first set bit, in a non-zero bitmask.
-        macro_rules! bsf {
-            ($mask:expr) => {
-                match ($mask).get() {
-                    #[allow(unsafe_unsafe)]
-                    0 => unsafe { ::core::hint::unreachable_unchecked() },
-                    #[cfg(target_endian = "little")]
-                    mask => mask.trailing_zeros(),
-                    #[cfg(target_endian = "big")]
-                    mask => mask.leading_zeros(),
-                }
-            };
+    };
+}
+/// Search forward for the first set bit, in a non-zero bitmask.
+#[cfg(not(rustc_1_53))]
+macro_rules! bsf {
+    ($mask:expr) => {
+        match ($mask).get() {
+            #[allow(unsafe_unsafe)]
+            0 => unsafe { ::core::hint::unreachable_unchecked() },
+            #[cfg(target_endian = "little")]
+            mask => mask.trailing_zeros(),
+            #[cfg(target_endian = "big")]
+            mask => mask.leading_zeros(),
         }
-    }
+    };
 }
 
-cfg_if::cfg_if! {
-    if #[cfg(feature = "std")] {
-        /// Test at runtime whether a CPU feature is available on x86/x86_64 platforms.
-        macro_rules! is_x86_feature_detected {
-            ($feature:tt) => {
-                ::std::is_x86_feature_detected!($feature)
-            };
-        }
-    } else if #[cfg(feature = "unstable")] {
-        /// Test at runtime whether a CPU feature is available on x86/x86_64 platforms.
-        macro_rules! is_x86_feature_detected {
-            ($feature:tt) => {
-                ::std_detect::is_x86_feature_detected!($feature)
-            };
-        }
-    } else {
-        /// Test at runtime whether a CPU feature is available on x86/x86_64 platforms.
-        macro_rules! is_x86_feature_detected {
-            ($feature:tt) => {
-                ::core::cfg!(target_feature = $feature)
-            };
-        }
-    }
+/// Test at runtime whether a CPU feature is available on x86/x86_64 platforms.
+#[cfg(feature = "std")]
+macro_rules! is_x86_feature_detected {
+    ($feature:tt) => {
+        ::std::is_x86_feature_detected!($feature)
+    };
+}
+/// Test at runtime whether a CPU feature is available on x86/x86_64 platforms.
+#[cfg(all(not(feature = "std"), feature = "unstable"))]
+macro_rules! is_x86_feature_detected {
+    ($feature:tt) => {
+        ::std_detect::is_x86_feature_detected!($feature)
+    };
+}
+/// Test at runtime whether a CPU feature is available on x86/x86_64 platforms.
+#[cfg(not(any(feature = "std", feature = "unstable")))]
+macro_rules! is_x86_feature_detected {
+    ($feature:tt) => {
+        ::core::cfg!(target_feature = $feature)
+    };
 }
